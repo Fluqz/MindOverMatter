949003d5d79e92e58976d2742b3d8d07ccd349a8
Robin <robin@hlawinka.de>
2017-01-31 00:05:44 +0100

    stuff

---
+++ Mind Over Matter Unity/Assets/Scripts/Enemy/Enemies/CrawlingGuy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrawlingGuy : Enemy {
+
+    private const string name = "Crawling Guy",
+                    description = "A slow creature that will shoot in the ground to make a explosion.";
+    private const int health = 8,
+                        weight = 40,
+                        baseDamage = 100,
+                        degree = 3,
+                        criticalStrikeChance = 30;
+    private const float attackRadius = 3,
+                        terretoryRadius = 10,
+                        movementSpeed = 5f;
+
+    public CrawlingGuy()
+        : base(new EnemyBaseInformation(name, description, health, weight, baseDamage, attackRadius, terretoryRadius, movementSpeed, degree), criticalStrikeChance) {
+
+        this.EnemyAbilities.Add(new EnergyShot());
+    }
+}
