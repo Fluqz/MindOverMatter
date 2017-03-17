9e028929d835cbb46e40b285c13b92d4329d9464
Patrick <baesslerpa@gmail.com>
2017-03-17 15:19:00 +0100

    will eig nur pullen

--- Mind Over Matter Unity/Assets/Scripts/Pat_geklaut/PlayAnimationRandom.cs
+++ Mind Over Matter Unity/Assets/Scripts/Pat_geklaut/PlayAnimationRandom.cs
@@ -10,11 +10,6 @@ public class PlayAnimationRandom : MonoBehaviour {
 		AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);//could replace 0 by any other animation layer index
 		anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
 
 
 }
