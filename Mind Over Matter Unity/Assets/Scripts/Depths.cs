517d7299cc895355b32d76b3497430b1925e10f5
Robin <robin@hlawinka.de>
2017-03-20 23:01:16 +0100

    stuff

---
+++ Mind Over Matter Unity/Assets/Scripts/Depths.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depths : MonoBehaviour {
+    	
+	void Start () {
+        if(this.gameObject.GetComponent<Rigidbody2D>())
+            InvokeRepeating("UpdateDepths", 0, 0.1f);
+	}
+
+    void UpdateDepths() {
+        this.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
+    }
+}
